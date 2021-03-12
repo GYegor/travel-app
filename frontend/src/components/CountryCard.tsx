20ba8199c86ce615a8cc4cfd517ac98c0c1ff539
Palina Cetin <fpe@tut.by>
2021-03-12 12:56:01 +0300

    fix: fixed cards according the task

--- frontend/src/components/CountryCard.tsx
+++ frontend/src/components/CountryCard.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-
+import '../styles/CountryCard.scss';
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
+import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core";
 import { Link } from "react-router-dom";
@@ -13,22 +12,11 @@ import { theme } from "../mui-style";
 
 const useStyles = makeStyles({
   root: {
-    maxWidth: 345,
+    maxWidth: 300,
   },
   media: {
-    height: 140,
-  },
-  link: {
-    textDecoration: "none",
-    padding: theme.spacing(0.3, 0),
-  },
-  content: {
-    backgroundColor: 'rgb(248, 255, 247)',
-  },
-  buttonCont: {
-    justifyContent: 'center',
-    backgroundColor: 'rgb(248, 255, 247)',
-  }
+    height: 150,
+  }, 
 });
 
 const CountryCard: React.FC<ICardProps> = (props) => {
@@ -37,28 +25,24 @@ const CountryCard: React.FC<ICardProps> = (props) => {
 
   return (
     <Card className={classes.root}>
-      <CardMedia
-        className={classes.media}
-        component="img"
-        alt={country}
-        image={imageUrl}
-      />
-      <CardContent className={classes.content}>
-        <Typography gutterBottom variant="h5" component="h2">
-          {country}
-        </Typography>
-        <Typography variant="body2" color="textSecondary" component="p">
-          {capital}
-        </Typography>
-      </CardContent>
-
-      <CardActions className={classes.buttonCont}>
-        <Link to={`/country`} className={classes.link}>
-          <Button variant="contained" size="small" color="secondary">
-            Learn More
-          </Button>
-        </Link>
-      </CardActions>
+      <Link to={`/country`} className="card__link">
+        <CardActionArea className="card">
+          <CardMedia
+            className={classes.media}
+            component="img"
+            alt={country}
+            image={imageUrl}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {country}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {capital}
+            </Typography>
+          </CardContent>
+        </CardActionArea>                  
+      </Link>      
     </Card>
   );
 };
