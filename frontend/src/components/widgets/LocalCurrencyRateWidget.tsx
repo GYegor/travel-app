ea53dffb10cfaeab140fdedf45ac289f9fae0604
MarinaYur <marina.yurkev@gmail.com>
2021-03-16 02:00:34 +0300

    add dinamic change of currency rates

--- frontend/src/components/widgets/LocalCurrencyRateWidget.tsx
+++ frontend/src/components/widgets/LocalCurrencyRateWidget.tsx
@@ -6,7 +6,7 @@ import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import { createStyles, makeStyles, Theme } from "@material-ui/core";
 import { fade } from '@material-ui/core/styles/colorManipulator';
-import { AppState, Language } from "../../interfaces";
+import { AppState, ICountry, Language } from "../../interfaces";
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
   root: {
@@ -24,79 +24,91 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
   },
 }));
 
-const TARGET_CURRENCY = 'EUR';
+// const TARGET_CURRENCY = 'EUR';
 
-interface Irates {
-  string: number; //так будет проблемно
-  // динамически вставлять переменную тоже не очень.
-}
+// interface Irates {
+//   string: number; //так будет проблемно
+//   // динамически вставлять переменную тоже не очень.
+// }
 
 const LocalCurrencyRateWidget: React.FC = () => {
   const classes = useStyles();
-  const lang = useSelector<AppState>(state => state.lang);
+  const { lang, country } = useSelector<AppState, AppState>(state => state);
   const api = {
     key: '966802a9f2ca87f80cfe',
     base: 'https://free.currconv.com/api/v7/'
   }
-  
+
   const [rates, setRates] = useState<any>(null);
   const [ratesEuro, setRatesEuro] = useState<any>(null);
-
   useEffect(() => {
-    fetch(`${api.base}convert?q=USD_${symbolsOfRate.Spain},RUB_${symbolsOfRate.Spain}&compact=ultra&apiKey=${api.key}`)
+    if (!country) {
+      return;
+    }
+    const localCurrency = (country as ICountry).currencyCode;
+    fetch(`${api.base}convert?q=USD_${localCurrency},RUB_${localCurrency}&compact=ultra&apiKey=${api.key}`)
       .then(res => res.json())
       .then(result => {
         setRates(result);
       });
-  }, []);
+  }, [country]);
 
   useEffect(() => {
-    fetch(`${api.base}convert?q=EUR_${symbolsOfRate.Turkey}&compact=ultra&apiKey=${api.key}`)
+    if (!country) {
+      return;
+    }
+    const localCurrency = (country as ICountry).currencyCode;
+
+    fetch(`${api.base}convert?q=EUR_${localCurrency}&compact=ultra&apiKey=${api.key}`)
       .then(res => res.json())
       .then(result => {
         setRatesEuro(result);
       });
-  }, []);
+  }, [country]);
 
 
   console.log('rates', rates);
   console.log('ratesEuro', ratesEuro);
-
-  const symbolsOfRate = {
-    Italy: 'EUR',
-    Turkey: 'TRY',
-    Spain: 'EUR',
-    Poland: 'PLN',
-    Germany: 'EUR',
-    China: 'CNY',
-    England: 'GBP',
-    Egypt: 'EGP',
-  }
   const currencyRateInformation = {
     [Language.en]: ['Currency rates', '1 USD', '1 EUR', '100 RUB'],
     [Language.ru]: ['Курсы валют', '1 доллар США', '1 евро', '100 российских рублей'],
     [Language.by]: ['Курсы валют', '1 долар ЗША', '1 еўра', '100 расійскіх рублёў']
   }
 
+  function getRate(currency: 'USD' | 'EUR' | 'RUB'): string {
+    if (!country || !rates || !ratesEuro) {
+      return '';
+    }
+    const index = currency + '_' + (country as ICountry).currencyCode;
+    if(currency === 'EUR') {
+      return ratesEuro[index] + ' ' + (country as ICountry).currencyCode;
+    }
+    // if(currency === 'RUB') {
+    //   return ratesEuro[index] + ' ' + (country as ICountry).currencyCode;
+    // }
+    return rates[index] + ' ' + (country as ICountry).currencyCode;
+  }
+
   return (
-    <Card className={classes.root}>
+    country && (<Card className={classes.root}>
       <CardActionArea className={classes.action}>
         <CardContent>
-      <Typography variant="h5" component="h3">
-        {currencyRateInformation[lang as Language][0]}
-      </Typography>
-          <Typography variant="body1" component="h3">
-           <span style={{color:"rgba(0, 0, 0, 0.54)"}}>1 USD = </span>{rates?.USD_EUR} EUR
+          <Typography variant="h5" component="h3">
+            {currencyRateInformation[lang as Language][0]}
           </Typography>
           <Typography variant="body1" component="h3">
-          <span style={{color:"rgba(0, 0, 0, 0.54)"}}>1 EUR = </span>{symbolsOfRate.Spain==="EUR" ? 1 : ratesEuro?.EUR_EUR} EUR
+            <span style={{ color: "rgba(0, 0, 0, 0.54)" }}>1 USD = </span>
+            {getRate('USD')}
           </Typography>
           <Typography variant="body1" component="h3">
-          <span style={{color:"rgba(0, 0, 0, 0.54)"}}>100 RUB = </span>{(rates?.RUB_EUR*100).toFixed(4)} EUR
+            <span style={{ color: "rgba(0, 0, 0, 0.54)" }}>1 EUR = </span>{getRate('EUR')}
+          </Typography>
+          <Typography variant="body1" component="h3">
+            <span style={{ color: "rgba(0, 0, 0, 0.54)" }}>100 RUB = </span>{getRate('RUB')}
           </Typography>
         </CardContent>
       </CardActionArea>
-    </Card>
+    </Card>)
   );
 };
 
