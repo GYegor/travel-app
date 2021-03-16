73d6bc1c5aca5a45f5acf3c62909a8cc7687b4ce
MarinaYur <marina.yurkev@gmail.com>
2021-03-16 16:51:54 +0300

    feat: add information about Poland and Germany

---
+++ frontend/src/data/moreData/germany.ts
@@ -0,0 +1,162 @@
+const Germany = {
+	countryId: 4,
+	smallImageId: "germany.jpg",
+	imageId: "germany_full.jpg",
+	videoUrl: "https://youtu.be/_7Zytt5SZek",
+	coords: [52.5187, 13.3747],
+	localTimeDiff: 1,
+	sights: [
+		{
+			sightId: 1,
+			imageId: "1-reichstag_building-kopiya.jpg",
+			smallImageId: "",
+			localizations: [
+				{
+					name: "Reichstag building",
+					description:
+						"The majestic palace of German statehood, built in 1894, has witnessed all the historical events that shaped the fate of modern Germany. The building of the German Parliament was destroyed more than once, it was badly damaged in a fire in 1933, and as a result of the bombing in 1945 it was practically destroyed. However, after several reconstructions, the structure acquired its original appearance, and a grandiose glass dome was also restored, where a unique observation deck is located, accessible to tourists. From here, in clear weather, you can see the surrounding sights of Berlin in all its glory."
+				},
+				{
+					name: "Здание Рейхстага",
+					description:
+						"Величественный дворец немецкой государственности, построенный в 1894 году, стал свидетелем всех исторических событий, сформировавших судьбу современной Германии. Здание немецкого Парламента не раз подвергалось разрушениям, оно сильно пострадало при пожаре в 1933 году, а в результате бомбардировок в 1945 году было практически разгромлено. Однако, после нескольких реконструкций сооружение обрело первоначальный облик, также был восстановлен грандиозный стеклянный купол, где расположена уникальная смотровая площадка, доступная для туристов. Отсюда в ясную погоду можно увидеть окрестные достопримечательности Берлина во всей красе."
+				},
+				{
+					name: "Будынак Рэйхстага",
+					description:
+						"Велічны палац нямецкай дзяржаўнасці, пабудаваны ў 1894 годзе, стаў сведкам усіх гістарычных падзей, што сфармавалі лёс сучаснай Германіі. Будынак нямецкага Парламента не раз падвяргалася разбурэнняў, яно моцна пацярпела пры пажары ў 1933 годзе, а ў выніку бамбаванняў ў 1945 годзе было практычна разгромлена. Аднак, пасля некалькіх рэканструкцый збудаванне здабыла першапачатковае аблічча, таксама быў адноўлены грандыёзны шкляны купал, дзе размешчана унікальная назіральная пляцоўка, даступная для турыстаў. Адсюль у яснае надвор'е можна ўбачыць навакольныя славутасці Берліна ва ўсёй красе."
+				},
+			],
+		},
+		{
+			sightId: 2,
+			imageId: "2-brandenbur_gate.jpg",
+			smallImageId: "",
+			localizations: [
+				{
+					name: "Brandenburg Gate",
+					description:
+						"The classic arch of the Brandenburg Gate, as conceived by the architects, is the Gate of Peace, but ironically, German soldiers went through them to fight. Four horses, ruled by the goddess of victory, are crowned with twelve columns. In this form, the Brandenburg Gate became a symbol of a united Germany and was replicated on millions of postcards that were scattered around the world. Tourists contemplating what to visit in Berlin head through these gates to the famous Linden Alley and move along it, exploring the historical sites. In the photographs of this part of the city, the Brandenburg Gate is sure to get into the frame, bringing the flavor of the German capital to the landscape."
+				},
+				{
+					name: "Бранденбургские ворота",
+					description:
+						"Классическая арка Бранденбургских ворот по замыслу архитекторов являет собой Врата мира, но по иронии судьбы через них немецкие солдаты отправлялись воевать. Четверка лошадей, управляемая богиней победы, венчает двенадцать колонн. В таком виде Бранденбургские ворота стали символом объединенной Германии и были растиражированы на миллионах открыток, разлетевшихся по всему миру. Туристы, размышляющие, что посетить в Берлине, направляются через эти ворота к знаменитой Липовой аллее и продвигаются по ней, осматривая исторические места. На фотоснимках этой части города Бранденбургские ворота обязательно попадают в кадр, привнося в пейзаж колорит немецкой столицы."
+				},
+				{
+					name: "Brandenburg Gate",
+					description:
+						"Класічная арка Брадэнбургскіх варот паводле задумы архітэктараў уяўляе сабой Брама свету, але па іроніі лёсу праз іх нямецкія салдаты адпраўляліся ваяваць. Чацвёрка коней, кіраваная багіняй перамогі, вянчае дванаццаць калон. У такім выглядзе Брадэнбургскіх вароты сталі сімвалам аб'яднанай Германіі і былі растыражаваныя на мільёнах паштовак, рассыпаныя па ўсім свеце. Турысты, разважала, што наведаць у Берліне, накіроўваюцца праз гэтую браму да знакамітай Ліпавым алеі і прасоўваюцца па ёй, аглядаючы гістарычныя месцы. На фотаздымках гэтай частцы горада Брадэнбургскіх вароты абавязкова трапляюць у кадр, прыўносячы ў пейзаж каларыт нямецкай сталіцы."
+				},
+			],
+		},
+		{
+			sightId: 3,
+			imageId: "3-museum_island.jpg",
+			smallImageId: "",
+			localizations: [
+				{
+					name: "Museum Island",
+					description:
+						"On an island in the middle of the Spree River, there are 5 Berlin museums. This historical complex began its development from the first building, built in 1830, called the Old Museum, which displays an exhibition of antique statues, weapons and ancient Greek jewelry. Later, a New Museum was built, where samples of papyri and exhibits of ancient Egyptian art are exhibited. The Old National Gallery, opened in 1876, will be of interest to lovers of impressionism."
+				},
+				{
+					name: "Музейный остров",
+					description:
+						"На острове посреди реки Шпрее расположились 5 берлинских музеев. Этот исторический комплекс начал свое развитие от первого построенного в 1830 году здания, названного Старым музеем, в котором представлена экспозиция античных статуй, оружия и древнегреческих украшений. Позднее был построен и Новый музей, где выставлены образцы папирусов и экспонаты древнеегипетского искусства. Старая национальная галерея, открытая в 1876 году, будет интересна любителям импрессионизма."
+				},
+				{
+					name: "Музейны востраў",
+					description:
+						"На востраве пасярод ракі Шпрее размясціліся 5 берлінскіх музеяў. Гэты гістарычны комплекс пачаў сваё развіццё ад першага пабудаванага ў 1830 годзе будынка, названага Старым музеем, у якім прадстаўлена экспазіцыя антычных статуй, зброі і старажытнагрэцкіх упрыгожванняў. Пазней быў пабудаваны і Новы музей, дзе выстаўлены ўзоры папірусаў і экспанаты старажытнаегіпецкага мастацтва. Старая нацыянальная галерэя, адкрытая ў 1876 годзе, будзе цікавая аматарам імпрэсіянізму."
+				},
+			],
+		},
+		{
+			sightId: 4,
+			imageId: "4-berlin_cathedra.jpg",
+			smallImageId: "",
+			localizations: [
+				{
+					name: "Berlin Cathedral",
+					description:
+						"The majestic Protestant temple was built for 11 years. He was assigned the role of the main church of all Lutherans. The baroque building of the cathedral solemnly rises on the Museum Island, and the interior decoration does not overwhelm, as in most Catholic cathedrals, but encourages you to carefully examine the details of sculptures, stained glass windows, paintings with scenes from the Bible. Representatives of the Hohenzollern dynasty are buried in the tomb of the cathedral."
+				},
+				{
+					name: "Берлинский кафедральный собор",
+					description:
+						"Величественный протестантский храм возводили 11 лет. Ему отводилась роль главной церкви всех лютеран. Барочное здание собора торжественно возвышается на Музейном острове, а внутреннее убранство не подавляет, как в большинстве католических соборов, а побуждает внимательно разглядеть детали скульптур, витражей, картин с сюжетами из библии. В усыпальнице собора покоятся представители династии Гогенцоллернов."
+				},
+				{
+					name: "Берлінскі кафедральны сабор",
+					description:
+						"Велічны пратэстанцкі храм ўзводзілі 11 гадоў. Яму адводзілася роля галоўнай царквы ўсіх лютэран. Барочнай будынак саборы ўрачыста узвышаецца на Музейным востраве, а ўнутранае ўбранне не падаўляе, як у большасці каталіцкіх сабораў, а падахвочвае ўважліва разгледзець дэталі скульптур, вітражоў, карцін з сюжэтамі з бібліі. У магільным склепе сабора спачываюць прадстаўнікі дынастыі Гогенцолернаў."
+				},
+			],
+		},
+		{
+			sightId: 5,
+			imageId: "5-alte_nationalgaleriel.jpg",
+			smallImageId: "",
+			localizations: [
+				{
+					name: "Old National Gallery",
+					description:
+						"In the antique building of the old national gallery, painting lovers can get acquainted with the richest collection of paintings of various directions, created in the 19th century. The names of Menzel, beloved by the Germans, as well as Manet and Monet, Liebermann, Beklin attract specialists and those interested in the gallery like a magnet. The works of European sculptors also have a significant place in the collection of this museum."
+				},
+				{
+					name: "Старая Национальная галерея",
+					description:
+						"В античном здании старой национальной галереи любители живописи смогут ознакомиться с богатейшей коллекцией картин различных направлений, созданных в XIX веке. Имена любимого немцами Менцеля, а также Мане и Моне, Либерманна, Беклина притягивают в галерею специалистов и интересующихся словно магнит. Работам европейских скульпторов также отводится значительное место в собрании этого музея."
+				},
+				{
+					name: "Старая Нацыянальная галерэя",
+					description:
+						"У антычным будынку старой нацыянальнай галерэі аматары жывапісу змогуць азнаёміцца ​​з найбагатай калекцыяй карцін розных напрамкаў, створаных у XIX стагоддзі. Імёны любімага немцамі Менцаля, а таксама Мане і Моне, Ліберманам, Беклина прыцягваюць у галерэю спецыялістаў і цікавяцца нібы магніт. Прац еўрапейскіх скульптараў таксама адводзіцца значнае месца ў сходзе гэтага музея."
+				},
+			],
+		},
+		{
+			sightId: 6,
+			imageId: "6-AquaDom.jpg",
+			smallImageId: "",
+			localizations: [
+				{
+					name: "AquaDom",
+					description:
+						"It is in Berlin that the world's largest salt water aquarium is located - a giant cylindrical tank called the Aquadom. Particularly lucky are the guests of the five-star Radisson Blu Hotel, whose windows overlook the courtyard-well with this incredible aquarium. The spectacle is impressive - 2,500 fish swim smoothly among realistic corals, caves, rocks and algae, illuminated by a pleasant blue light. The height of the glass structure is 25 meters, the diameter is 11 meters, and through the thick 19-centimeter glass you can observe the inhabitants not only from the outside, but also in the inner cylinder, from the transparent elevator passing here."
+				},
+				{
+					name: "АкваДом",
+					description:
+						"Именно в Берлине располагается крупнейший в мире аквариум с солёной водой — гигантский цилиндрический резервуар под названием Аквадом. Особенно повезло постояльцам пятизвёздочного отеля Рэдиссон Блу, окна которого выходят во внутренний двор-колодец с этим невероятным аквариумом. Зрелище впечатляющее — 2,5 тысячи рыб плавно проплывают среди реалистичных кораллов, пещер, скал и водорослей, подсвеченных приятным голубым светом. Высота стеклянного сооружения составляет 25 метров, диаметр 11 метров, а сквозь толстое 19-сантиметровое стекло можно наблюдать обитателей не только снаружи, но и во внутреннем цилиндре, из проходящего здесь прозрачного лифта."
+				},
+				{
+					name: "АкваДом",
+					description:
+						"Менавіта ў Берліне размяшчаецца найбуйнейшы ў свеце акварыум з салёнай вадой - гіганцкі цыліндрычны рэзервуар пад назвай Аквадзіў. Асабліва пашанцавала пастаяльцам пяцізоркавага гатэля Рэдиссон Блу, вокны якога выходзяць ва ўнутраны двор-калодзеж з гэтым неверагодным акварыумам. Відовішча ўражлівае - 2,5 тысячы рыб плаўна праплываюць сярод рэалістычных каралаў, пячор, скал і водарасцяў, падсветленых прыемным блакітным святлом. Вышыня шклянога збудаванні складае 25 метраў, дыяметр 11 метраў, а скрозь тоўстае 19-сантыметровы шкло можна назіраць насельнікаў не толькі звонку, але і ва ўнутраным цыліндры, з які праходзіць тут празрыстага ліфта."
+				},
+			],
+		},
+	],
+	localizations: [
+		{
+			name: "Germany",
+			capital: "Berlin",
+			description:
+				"Germany is a state in Western Europe with forests, rivers, mountain ranges and beach resorts in the North Sea. The history of the country covers a period of more than two thousand years. The capital, Berlin, is full of cultural and nightlife. Many sights of the city, in particular the Brandenburg Gate, remind of the events of the Second World War. Munich is famous for its Oktoberfest and beer restaurants. The most famous of them is the Hofbräuhaus, which opened in the 16th century. Frankfurt is a city of skyscrapers, it is here that the headquarters of the European Central Bank is located."
+		},
+		{
+			name: "Германия",
+			capital: "Берлин",
+			description:
+				"Германия – государство в Западной Европе с лесами, реками, горными хребтами и пляжными курортами Северного моря. История страны охватывает период в более чем две тысячи лет. В столице, Берлине, кипит культурная и ночная жизнь. Многие достопримечательности города, в частности Бранденбургские ворота, напоминают о событиях Второй мировой войны. Мюнхен знаменит фестивалем Октоберфест и пивными ресторанами. Самый известный из них – 'Хофбройхаус', открывшийся ещё в XVI веке. Франкфурт – город небоскребов, именно здесь находится штаб-квартира Европейского центрального банка."
+		},
+		{
+			name: "Германія",
+			capital: "Берлін",
+			description:
+				"Германія - дзяржава ў Заходняй Еўропе з лясамі, рэкамі, горнымі хрыбтамі і пляжнымі курортамі Паўночнага мора. Гісторыя краіны ахоплівае перыяд у больш чым дзве тысячы гадоў. У сталіцы, Берліне, кіпіць культурная і начное жыццё. Многія славутасці горада, у прыватнасці Брадэнбургскіх вароты, нагадваюць пра падзеі Другой сусветнай вайны. Мюнхен знакаміты фестывалем Октоберфест і піўнымі рэстаранамі. Самы вядомы з іх - 'Хофбройхаус', які адкрыўся яшчэ ў XVI стагоддзі. Франкфурт - горад хмарачосаў, менавіта тут знаходзіцца штаб-кватэра Еўрапейскага цэнтральнага банка."
+		},
+	],
+};
---
+++ frontend/src/data/moreData/poland.ts
@@ -0,0 +1,162 @@
+const Poland = {
+  countryId: 3,
+  smallImageId: "poland.jpg",
+  imageId: "poland_full.jpg",
+  videoUrl: "https://youtu.be/QAsT2d7IP3c",
+  coords: [52.2321, 21.0071],
+  localTimeDiff: 1,
+  sights: [
+    {
+      sightId: 1,
+      imageId: "1-malbork_castle.jpg",
+      smallImageId: "",
+      localizations: [
+        {
+          name: "Marienburg castle",
+          description:
+            "'Nest' of German knights. The brick bulk of this castle rises on the banks of the Nogat River, which flows 80 km from the border with the Kaliningrad region of the Russian Federation. Its history began 700 years ago, when the Teutonic residence was officially moved from Venice to Marienburg. The fortress was repeatedly completed and decorated. In 1945, the castle was severely damaged, but after the end of World War II, it was literally rebuilt from scratch. Now there is a museum within the walls of the building, where interesting weapons collections, amber jewelry, and armor are presented."
+        },
+        {
+          name: "Замок Мариенбург",
+          description:
+            "«Гнездо» немецких рыцарей. Кирпичная громада этого замка возвышается на берегу реки Ногат, которая протекает в 80 км от границы с Калининградской областью РФ. Его история началась еще 700 лет назад, когда резиденция тевтонов официально была перенесена из Венеции в Мариенбург. Крепость неоднократно достраивалась, украшалась. В 1945 году замок подвергся большим разрушениям, но после окончания Второй мировой войны его отстроили буквально заново. Сейчас в стенах здания функционирует музей, там представлены интересные оружейные коллекции, янтарные украшения, доспехи."
+        },
+        {
+          name: "замак Марыенбург",
+          description:
+            "«Гняздо» нямецкіх рыцараў. Цагляная грамада гэтага замка ўзвышаецца на беразе ракі Ногат, якая працякае ў 80 км ад мяжы з Калінінградскай вобласцю РФ. Яго гісторыя пачалася яшчэ 700 гадоў таму, калі рэзідэнцыя тэўтонаў афіцыйна была перанесена з Венецыі ў Марыенбург. Крэпасць неаднаразова дабудоўвалася, ўпрыгожвалася. У 1945 годзе замак падвергнуўся вялікім разбурэнняў, але пасля заканчэння Другой сусветнай вайны яго адбудавалі літаральна зноўку. Зараз у сценах будынка функцыянуе музей, там прадстаўлены цікавыя зброевыя калекцыі, бурштынавыя ўпрыгожванні, даспехі."
+        },
+      ],
+    },
+    {
+      sightId: 2,
+      imageId: "2-tatry.jpg",
+      smallImageId: "",
+      localizations: [
+        {
+          name: "Tatras",
+          description:
+            "This is the name of the mountain system of the Carpathians, the highest point of which reaches 2499 m. This place attracts tourists not only with heights within 1800-2500 m, but also with glacial circuses, mountain lakes in large numbers, deep valleys. The rating of this attraction in Poland is assessed as high, all because there is still a ski resort here, known as Zakopane. You can get here from the capital by train. The Tatras are also famous for their caves, of which there are really many."
+        },
+        {
+          name: "Татры",
+          description:
+            "Так называется горная система Карпат, высшая точка которой достигает 2499 м. Это место привлекает туристов не только высотами в пределах 1800-2500 м, но еще ледниковыми цирками, горными озерами в большом количестве, глубокими долинами. Рейтинг этой достопримечательности Польши оценивается как высокий, все потому что здесь еще есть горнолыжный курорт, известный под названием Закопане. Из столицы сюда можно добраться на поезде. Еще Татры известны пещерами, которых здесь действительно много."
+        },
+        {
+          name: "Татры",
+          description:
+            "Так завецца горная сістэма Карпат, вышэйшая кропка якой дасягае 2499 м. Гэта месца прыцягвае турыстаў не толькі вышынямі ў межах 1800-2500 м, але яшчэ ледавіковымі цыркамі, горнымі азёрамі ў вялікай колькасці, глыбокімі далінамі. Рэйтынг гэтай славутасці Польшчы ацэньваецца як высокі, усё таму што тут яшчэ ёсць гарналыжны курорт, вядомы пад назвай Закапанэ. Са сталіцы сюды можна дабрацца на цягніку. Яшчэ Татры вядомыя пячорамі, якіх тут сапраўды шмат."
+        },
+      ],
+    },
+    {
+      sightId: 3,
+      imageId: "3-krakow_old_town.jpg",
+      smallImageId: "",
+      localizations: [
+        {
+          name: "Krakow Old Town",
+          description:
+            "If you are planning to go exactly to Krakow, it is better to start its survey from the Old Town, all roads from which lead to the market square. It is a 'front living room', both locals and guests of the country are used to gathering there. Krakow residents are proud of their square, because it is considered one of the largest in Europe, occupying 200 * 200 m. A trip to the Old Town is worth it, because the market ensemble has preserved in detail the planning of the times when trade with all of Europe was carried out."
+        },
+        {
+          name: "Старый город Кракова",
+          description:
+            "Если вы планируете отправиться именно в Краков, начать его обзор лучше со Старого города, все дороги с которого ведут к рыночной площади. Она представляет собой «парадную гостиную», на ней привыкли собираться как местные жители, так и гости страны. Краковцы гордятся своей площадью, потому что она считается одной из самых больших в Европе, занимает 200*200 м. Путешествие в Старый город стоит того, потому что рыночный ансамбль сохранил в деталях планировку времен, когда торговля осуществлялась со всей Европой."
+        },
+        {
+          name: "Стары горад Кракава",
+          description:
+            "Калі вы плануеце адправіцца менавіта ў Кракаў, пачаць яго агляд лепш са Старога горада, усе дарогі з якога вядуць да рынкавай плошчы. Яна ўяўляе сабой «парадную гасціную», на яе прывыклі збірацца як мясцовыя жыхары, так і госці краіны. Краковцы ганарацца сваёй плошчай, таму што яна лічыцца адной з самых вялікіх у Еўропе, займае 200 * 200 м. Падарожжа ў Стары горад варта таго, таму што рынкавы ансамбль захаваў у дэталях планіроўку часоў, калі гандаль ажыццяўлялася з усёй Еўропай."
+        },
+      ],
+    },
+    {
+      sightId: 4,
+      imageId: "4-wooden_churches_of_southern_little_poland.jpg",
+      smallImageId: "",
+      localizations: [
+        {
+          name: "Wooden churches in the south of Lesser Poland",
+          description:
+            "Interesting places are located in the south and east of the Lesser Poland Voivodeship, there is a whole group of churches made of natural wood that are historically valuable from an architectural point of view. Most of them look like a log house, and some represent different architectural styles. The buildings fit perfectly into the picturesque mountain landscape. From the description of this sight of Poland, you can find out that it is included in the UNESCO list."
+        },
+        {
+          name: "Деревянные костелы юга Малой Польши",
+          description:
+            "Интересные места находятся на юге и востоке Малопольского воеводства, там есть целая группа исторически ценных с архитектурной точки зрения костелов из натурального дерева. Большинство из них имеют вид сруба, а некоторые представляют различные архитектурные стили. Постройки идеально вписываются в живописный горный пейзаж. Из описания этой достопримечательности Польши можно узнать, что она внесена в список ЮНЕСКО."
+        },
+        {
+          name: "Драўляныя касцёлы поўдня Малой Польшчы",
+          description:
+            "Цікавыя месцы знаходзяцца на поўдні і ўсходзе Малапольскага ваяводства, там ёсць цэлая група гістарычна каштоўных з архітэктурнай пункту гледжання касцёлаў з натуральнага дрэва. Большасць з іх маюць выгляд зруба, а некаторыя ўяўляюць розныя архітэктурныя стылі. Пабудовы ідэальна ўпісваюцца ў маляўнічы горны пейзаж. З апісання гэтай славутасці Польшчы можна даведацца, што яна занесеная ў спіс ЮНЕСКА."
+        },
+      ],
+    },
+    {
+      sightId: 5,
+      imageId: "5-the_masurian_lake_district.jpg",
+      smallImageId: "",
+      localizations: [
+        {
+          name: "Masurian Lake District",
+          description:
+            "This landmark of Poland is one of the most beautiful; tourists are delighted with the beauty of the local nature. Mazury is a historical and ethnographic region in the northern part of the country. There are about 4 thousand lakes in this region, most of which are connected by canals and rivers. The total area of ​​the Mazury is about 1,700 km2. Here you can ride sailboats, kayaks."
+        },
+        {
+          name: "Мазурское поозёрье",
+          description:
+            "Эта достопримечательность Польши относится к числу самых красивых, от красоты местной природы туристы приходят в восторг. Мазуры представляют собой исторический и этнографический район в северной части страны. В этом регионе насчитывается около 4 тысяч озер, большая часть которых соединена каналами, реками. Общая площадь Мазуров составляет около 1700 км2. Здесь можно покататься на парусниках, байдарках."
+        },
+        {
+          name: "Мазурскае поозёрье",
+          description:
+            "Гэтая славутасць Польшчы адносіцца да ліку самых прыгожых, ад прыгажосці мясцовай прыроды турысты прыходзяць у захапленне. Мазуры ўяўляюць сабой гістарычны і этнаграфічны раён у паўночнай частцы краіны. У гэтым рэгіёне налічваецца каля 4 тысяч азёр, вялікая частка якіх злучаная каналамі, рэкамі. Агульная плошча Мазураў складае каля 1700 км2. Тут можна пакатацца на ветразніках, байдарках."
+        },
+      ],
+    },
+    {
+      sightId: 6,
+      imageId: "6-slowinski_national_park.jpg",
+      smallImageId: "",
+      localizations: [
+        {
+          name: "Slovinsky National Park",
+          description:
+            "Wondering what to visit in Poland? Head to the town of eba, where there is an interesting place in the form of a parkland in the central part of the Polish coast. It was founded with the aim of preserving the natural beauty of the area. Meadows, a system of lakes, coastal forests, and unique dunes also needed protection. Reviews about this place are full of admiration; the park has picturesque lakes, into which sea water falls during strong storms. The tourist infrastructure here is assessed as well developed."
+        },
+        {
+          name: "Словинский национальный парк",
+          description:
+            "Раздумываете над тем, что посетить в Польше? Отправляйтесь в город Леба, где есть интересное место в виде парковой зоны в центральной части польского побережья. Он был основан с целью сохранения природной красоты местности. В защите нуждались также луга, система озер, приморские леса, уникальные в своем роде дюны. Отзывы об этом месте полны восхищения, в парке есть живописные озера, в которые морская вода попадает во время сильных бурь. Туристическая инфраструктура здесь оценивается как хорошо развитая."
+        },
+        {
+          name: "Словинский нацыянальны парк",
+          description:
+            "Разважаеце над тым, што наведаць у Польшчы? Адпраўляйцеся ў горад Леба, дзе ёсць цікавае месца ў выглядзе паркавай зоны ў цэнтральнай частцы польскага ўзбярэжжа. Ён быў заснаваны з мэтай захавання прыроднай прыгажосці мясцовасці. У абароне мелі патрэбу таксама лугі, сістэма азёр, прыморскія лесу, унікальныя ў сваім родзе выдмы. Водгукі аб гэтым месцы поўныя захаплення, у парку ёсць маляўнічыя азёры, у якія марская вада трапляе падчас моцных бур. Турыстычная інфраструктура тут ацэньваецца як добра развітая."
+        },
+      ],
+    },
+  ],
+  localizations: [
+    {
+      name: "Poland",
+      capital: "Warsaw",
+      description:
+        "Poland is an amazing country that has always conquered with its hospitality, rich history and ancient monuments. The Baltic coast of Poland will delight people who do not like the sweltering heat with its mild climate and sandy beaches, about 500 kilometers long and European service. Fans of skiing holidays in Poland will undoubtedly enjoy rest in the resorts of Zakopane and Karkonosze, Beskids and Tatras. You can plunge into the rich history of Poland by visiting numerous museums, including Nikolai Kapernik, cultural and natural monuments, of which 13 are included in the UNESCO World Heritage List."
+    },
+    {
+      name: " Польша",
+      capital: "Варшава",
+      description:
+        "Польша – это удивительная  страна, которая всегда покоряла своим гостеприимством, богатой историей и памятниками старины. Балтийское побережье Польши порадует людей, которые не любят изнуряющую жару своим мягким климатом и песчаными пляжами, протяженностью около 500 километров и европейским сервисом. Любителям горнолыжного отдыха в Польше без сомнения окажется по душе отдых на курортах Закопане и Карконоше, Бескидах и Татрах. Окунуться в богатую историю Польши вы сможете при посещении многочисленных музеев, в том числе Николая Каперника, памятников культуры и природы, из которых 13 занесены в  список Мирового наследия ЮНЕСКО."
+    },
+    {
+      name: "Польшча",
+      capital: "Варшава",
+      description:
+        "Польшча - гэта дзіўная краіна, якая заўсёды пакарала сваёй гасціннасцю, багатай гісторыяй і помнікамі даўніны. Балтыйскае ўзбярэжжа Польшчы парадуе людзей, якія не любяць гарачыню сваім мяккім кліматам і пясчанымі пляжамі, працягласцю каля 500 кіламетраў і еўрапейскім сэрвісам. Аматарам гарналыжнага адпачынку ў Польшчы без сумневу апынецца па душы адпачынак на курортах Закапанэ і Карконоше, Бескидах і Татрах. Акунуцца ў багатую гісторыю Польшчы вы зможаце пры наведванні шматлікіх музеяў, у тым ліку Мікалая Каперника, помнікаў культуры і прыроды, з якіх 13 занесены ў спіс Сусветнай спадчыны ЮНЕСКА."
+    },
+  ],
+};
