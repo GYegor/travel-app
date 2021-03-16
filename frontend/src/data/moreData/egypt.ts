ec6f36e78e9c009502c7e00014de920c23e98e61
MarinaYur <marina.yurkev@gmail.com>
2021-03-16 21:09:32 +0300

    feat: add information about England and Egypt

---
+++ frontend/src/data/moreData/egypt.ts
@@ -0,0 +1,162 @@
+const Egypt = {
+  countryId: 8,
+  smallImageId: "egypt.jpg",
+  imageId: "egipet-full.jpg",
+  videoUrl: "https://youtu.be/N9sDBUqZeaI",
+  coords: [30.0507, 31.2469],
+  localTimeDiff: 2,
+  sights: [
+    {
+      sightId: 1,
+      imageId: "1-The-Valley-of-the-Kings.jpg",
+      smallImageId: "",
+      localizations: [
+        {
+          name: "Valley of the Pharaohs (Luxor)",
+          description:
+            "In the 16th century BC. with the purpose of burying the rulers of Egypt, the Valley of the Kings was created - a place in a rocky gorge, where a number of tombs were organized. At one time, this place was secret and was strictly guarded by rangers who protected it from raids and robberies."
+        },
+        {
+          name: "Долина фараонов (г. Луксор)",
+          description:
+            "В 16 веке до н.э. с целью захоронения правителей Египта была создана Долина царей — место в скалистом ущелье, где был организован ряд гробниц. В своё время это место было тайным и строго охранялось смотрителями, которые защищали его от налётов и грабежа."
+        },
+        {
+          name: "Даліна фараонаў (г. Луксор)",
+          description:
+            "У 16 стагоддзі да н.э. з мэтай пахавання кіраўнікоў Егіпта была створана Даліна цароў - месца ў скалістым цясніну, дзе быў арганізаваны шэраг магільняў. У свой час гэта месца было таемным і строга ахоўвалася наглядчыкамі, якія абаранялі яго ад налётаў і рабавання."
+        },
+      ],
+    },
+    {
+      sightId: 2,
+      imageId: "2-Karnak-Temple-Complex.jpg",
+      smallImageId: "",
+      localizations: [
+        {
+          name: "Temple of Karnak",
+          description:
+            "The Karnak Temple is a huge temple complex measuring 1.5 kilometers by 0.7 kilometers, which includes 33 temples and halls - all this looks more like a kind of city than a building. This entire complex has undergone a number of changes and additions over the course of 2 millennia, because each of the pharaohs strove to complement and transform this splendor in his own way, make his own contribution and leave some kind of memory about the period of his reign."
+        },
+        {
+          name: "Храм Карнака",
+          description:
+            "Храм Карнака представляет собой огромный храмовый комплекс размером 1,5 километра на 0,7 километра, куда входит 33 храма и зала — всё это больше напоминает своеобразный город, чем здание. Весь этот комплекс подвергался ряду изменений и дополнений на протяжении 2 тысячелетий, ведь каждый из фараонов стремился по-своему дополнить и преобразить это великолепие, внести свою лепту и оставить некую память о периоде его правления."
+        },
+        {
+          name: "Храм Карнака",
+          description:
+            "Храм Карнака ўяўляе сабой велізарны храмавы комплекс памерам 1,5 кіламетра на 0,7 кіламетра, куды ўваходзіць 33 храма і залы - усё гэта больш нагадвае своеасаблівы горад, чым будынак. Увесь гэты комплекс падвяргаўся шэрагу змяненняў і дапаўненняў на працягу 2 тысячагоддзяў, бо кожны з фараонаў імкнуўся па-свойму дапоўніць і змяніць гэта хараство, унесці сваю лепту і пакінуць нейкую памяць пра перыяд яго кіравання."
+        },
+      ],
+    },
+    {
+      sightId: 3,
+      imageId: "3-Saint-Catherines-Monastery.jpg",
+      smallImageId: "",
+      localizations: [
+        {
+          name: "Monastery of St. Catherine",
+          description:
+            "The monastery includes a large temple - the Basilica of the Transfiguration, as well as 12 small chapels, a garden, a refectory and a huge library of manuscripts, which is inferior in value only to the Vatican library."
+        },
+        {
+          name: "Монастырь Святой Екатерины",
+          description:
+            "В монастырь входит большой храм — Базилика Преображения, а также 12 небольших часовен, есть сад, трапезная и огромная библиотека рукописей, по своей ценности уступающая разве что ватиканской библиотеке."
+        },
+        {
+          name: "Манастыр Святой Кацярыны",
+          description:
+            "У манастыр ўваходзіць вялікі храм - Базыліка Праабражэння, а таксама 12 невялікіх капліц, ёсць сад, трапезная і велізарная бібліятэка рукапісаў, па сваёй каштоўнасці якая саступае хіба што ватыканскай бібліятэцы."
+        },
+      ],
+    },
+    {
+      sightId: 4,
+      imageId: "4-Bibliotheca-Alexandrina.jpg",
+      smallImageId: "",
+      localizations: [
+        {
+          name: "Library Alexandrina",
+          description:
+            "The library building has an unusual glass and aluminum cylinder shape with a sloping dome. The library is designed for about 8 million books, and the collections of works stored in it are collected literally from all over the world. The main reading room has a huge area - about 70 thousand square meters. m on 11 levels cascading."
+        },
+        {
+          name: "Библиотека Александрина",
+          description:
+            "Здание библиотеки имеет необычную форму цилиндра из стекла и алюминия со скошенным куполом. Библиотека рассчитана примерно на 8 млн книг, а собрания трудов, хранящиеся в ней, собраны буквально со всех уголков света. Основной читальный зал имеет огромную площадь — около 70 тыс. кв. м на 11 уровнях, идущих каскадом."
+        },
+        {
+          name: "Бібліятэка Александрына",
+          description:
+            "Будынак бібліятэкі мае незвычайную форму цыліндру са шкла і алюмінія са скошаным купалам. Бібліятэка разлічана прыкладна на 8 млн кніг, а сходы прац, якія захоўваюцца ў ёй, сабраны літаральна з усіх куткоў святла. Асноўны чытальная зала мае велізарную плошчу - каля 70 тыс. Кв. м на 11 узроўнях, якія ідуць каскадам."
+        },
+      ],
+    },
+    {
+      sightId: 5,
+      imageId: "5-Coloured-Canyon.jpg",
+      smallImageId: "",
+      localizations: [
+        {
+          name: "Colored canyon",
+          description:
+            "The entire canyon consists of many labyrinths, and its total length is about 5 kilometers. The route starts at the top, and then a narrow winding path gradually descends you to the bottom of the gorge, the passage becomes narrower, and bizarre patterns created by nature itself are observed on the steep cliffs."
+        },
+        {
+          name: "Цветной каньон",
+          description:
+            "Весь каньон состоит из множества лабиринтов, а его общая протяжённость составляет около 5 километров. Маршрут начинается наверху, а затем узкая извилистая тропинка постепенно спускает вас на дно ущелья, проход становится всё более узким, а на отвесных скалах наблюдаются причудливые узоры, созданные самой природой."
+        },
+        {
+          name: "Каляровы каньён",
+          description:
+            "Увесь каньён складаецца з мноства лабірынтаў, а яго агульная працягласць складае каля 5 кіламетраў. Маршрут пачынаецца наверсе, а затым вузкая звілістая сцяжынка паступова спускае вас на дно цясніны, праход становіцца ўсё больш вузкім, а на стромых скалах назіраюцца мудрагелістыя ўзоры, створаныя самой прыродай."
+        },
+      ],
+    },
+    {
+      sightId: 6,
+      imageId: "6-Nile_River_Egypt.jpg",
+      smallImageId: "",
+      localizations: [
+        {
+          name: "Nile river",
+          description:
+            "All settlements are located in the Nile Valley, where practically the entire population of the country lives, because the Nile is the main source of fresh water here. Among other things, the main tourist route runs along the Nile - after all, it is here that the vast majority of antiquities are concentrated. Cruises are constantly organized on the waters of the river: a comfortable boat trip is an ideal way to get to know the country, its values ​​and culture."
+        },
+        {
+          name: "Река Нил",
+          description:
+            "В долине Нила располагаются все населённые пункты, где проживает практически всё население страны, ведь Нил здесь является главным источником пресной воды. Помимо прочего, вдоль Нила пролегает основной туристический маршрут — ведь именно здесь сконцентрировано подавляющее большинство памятников древности. По водам реки постоянно организуются круизы: комфортабельное путешествие на теплоходе — идеальный способ познакомиться со страной, её ценностями и культурой."
+        },
+        {
+          name: " Рака Ніл",
+          description:
+            "У даліне Ніла размяшчаюцца ўсе населеныя пункты, дзе пражывае практычна ўсё насельніцтва краіны, бо Ніл тут з'яўляецца галоўнай крыніцай прэснай вады. Апроч іншага, уздоўж Ніла ідзе асноўны турыстычны маршрут - бо менавіта тут сканцэнтравана пераважная большасць помнікаў старажытнасці. Па водах ракі пастаянна арганізуюцца круізы: камфартабельнае падарожжа на цеплаходзе - ідэальны спосаб пазнаёміцца ​​з краінай, яе каштоўнасцямі і культурай."
+        },
+      ],
+    },
+  ],
+  localizations: [
+    {
+      name: "Egypt",
+      capital: "Cairo",
+      description:
+        "Egypt is a fabulous land of pyramids, mummies, temples, pharaohs. The sights of Egypt beckon into the depths of the centuries, forcing you to plunge into the history of antiquity and immerse yourself in the spirit of those times. Modernity and antiquity wonderfully coexist here with each other, giving this land a bewitching magical look. The country is distinguished by its originality, loyalty to traditions and a characteristic oriental flavor. The dry and hot climate allows you to come here on vacation in any season: even in the winter months, the sea water temperature is above 20 degrees."
+    },
+    {
+      name: "Египет",
+      capital: "Каир",
+      description:
+        "Египет — сказочная земля пирамид, мумий, храмов, фараонов. Достопримечательности Египта манят вглубь веков, заставляя окунуться в историю древности и проникнуться духом тех времен. Современность и старина удивительным образом уживаются здесь друг с другом, придавая этой земле завораживающе волшебный вид. Страну отличает самобытность, верность традициям и характерный восточный колорит. Сухой и жаркий климат позволяет приезжать сюда на отдых в любой сезон: даже в зимние месяцы температура воды в море держится выше 20 градусов."
+    },
+    {
+      name: "Егіпет",
+      capital: "Каір",
+      description:
+        "Егіпет - казачная зямля пірамід, мумій, храмаў, фараонаў. Славутасці Егіпта вабяць углыб стагоддзяў, прымушаючы акунуцца ў гісторыю старажытнасці і пранікнуцца духам тых часоў. Сучаснасць і даўніна дзіўным чынам ўжываюцца тут адзін з адным, надаючы гэтай зямлі зачаравальна чароўны выгляд. Краіну адрознівае самабытнасць, вернасць традыцыям і характэрны ўсходні каларыт. Сухі і гарачы клімат дазваляе прыязджаць сюды на адпачынак у любы сезон: нават у зімовыя месяцы тэмпература вады ў моры трымаецца вышэй за 20 градусаў."
+    },
+  ],
+};
---
+++ frontend/src/data/moreData/england.ts
@@ -0,0 +1,162 @@
+const England = {
+  countryId: 7,
+  smallImageId: "england.jpg",
+  imageId: "england-full.jpg",
+  videoUrl: "https://youtu.be/Uy9sXRk7its",
+  coords: [51.5074, -0.1277],
+  localTimeDiff: 0,
+  sights: [
+    {
+      sightId: 1,
+      imageId: "1-windsor-castle.jpg",
+      smallImageId: "",
+      localizations: [
+        {
+          name: "Windsor castle",
+          description:
+            "The residence of the British monarchs, towering on a hill in the Thames Valley, is not accidental. It was built in 1070, and today it is considered the most romantic building in the country. Moreover, due to the size of 580 m. By 165 m., Windsor Castle got into the Guinness Book of Records as the largest castle in the world. If you don't know what to see in England in 1 day, head here - you will not be disappointed."
+        },
+        {
+          name: "Виндзорский замок",
+          description:
+            "Резиденция британских монархов, возвышающаяся на холме в долине Темзы, не случайна. Он был возведен в 1070 году, и сегодня считается самой романтической постройкой страны. Более того, благодаря размерам в 580 м. на 165 м., Виндзорский замок попал в Книгу Рекордов Гиннеса в качестве крупнейшего замка всего мира. Если вы не знаете, что посмотреть в Англии за 1 день, отправляйтесь сюда – вы не будете разочарованы."
+        },
+        {
+          name: "Віндзорскі замак",
+          description:
+            "Рэзідэнцыя брытанскіх манархаў, якая ўзвышаецца на ўзгорку ў даліне Тэмзы, не выпадковая. Ён быў узведзены ў 1070 годзе, і сёння лічыцца самай рамантычнай пабудовай краіны. Больш за тое, дзякуючы памерах у 580 м. На 165 м., Віндзорскія замак трапіў у Кнігу Рэкордаў Гінэса ў якасці найбуйнейшага замка усяго свету. Калі вы не ведаеце, што паглядзець у Англіі за 1 дзень, адпраўляйцеся сюды - вы не будзеце расчараваныя."
+        },
+      ],
+    },
+    {
+      sightId: 2,
+      imageId: "2-big-ben.jpg",
+      smallImageId: "",
+      localizations: [
+        {
+          name: "London Big-Ben",
+          description:
+            "The commemorative clock proclaims the most important events of the state: the coming of the New Year, the Minutes of Silence, etc. Until recently, it was Big Ben that had the largest dial on Earth - not a single photo or description can convey all the greatness of the main symbol of London."
+        },
+        {
+          name: "Лондонский Биг-Бен",
+          description:
+            "Памятные часы провозглашают о самых важных событиях государства: наступлении Нового года, Минутах Молчания и т.д. До недавнего времени именно у Биг-Бена был самый громадный циферблат на Земле – ни одно фото или описание не способны передать всего величия главного символа Лондона;"
+        },
+        {
+          name: "Лонданскі Біг-Бэн",
+          description:
+            "Памятныя гадзіны абвяшчаюць пра самыя важныя падзеі дзяржавы: надыходзе Новага года, Хвілінах Маўчання і г.д. Да нядаўняга часу менавіта ў Біг-Бэна быў самы велічэзны цыферблат на Зямлі - ні адно фота або апісанне не здольныя перадаць усяго велічы галоўнага сімвала Лондана."
+        },
+      ],
+    },
+    {
+      sightId: 3,
+      imageId: "3-lakeland.jpg",
+      smallImageId: "",
+      localizations: [
+        {
+          name: "Lake District National Park",
+          description:
+            "The Lake District is famous for its superb panoramic views and flourishing countryside, mountainous and lakeside landscapes. The nature here is so unpredictable that in one day it can show all 4 seasons."
+        },
+        {
+          name: "Национальный парк Озерный край",
+          description:
+            "Озерный край знаменит благодаря своим превосходным панорамным видам и цветущей сельской местности, горным и озерным ландшафтам. Природа тут настолько непредсказуема, что за одни сутки может показать все 4 времени года."
+        },
+        {
+          name: "Нацыянальны парк Азёрны край",
+          description:
+            "Азёрны край знакаміты дзякуючы сваім выдатным панарамным відах і квітнеючай сельскай мясцовасці, горных і азёрным ландшафтам. Прырода тут настолькі непрадказальная, што за адны суткі можа паказаць усе 4 пары года."
+        },
+      ],
+    },
+    {
+      sightId: 4,
+      imageId: "4-hadrians-wall.jpg",
+      smallImageId: "",
+      localizations: [
+        {
+          name: "Hadrian's Val",
+          description:
+            "This historic landmark in England is 117 km long. It was erected in the years 122-128 by the Romans to protect against the raids of brigants and picts."
+        },
+        {
+          name: "Вал Адриана",
+          description:
+            "Длина этой исторической достопримечательности Англии составляет 117 км. Она возводилась в 122-128 годах римлянами для защиты от набегов бригантов и пиктов."
+        },
+        {
+          name: "вал Адрыяна",
+          description:
+            "Даўжыня гэтай гістарычнай славутасці Англіі складае 117 км. Яна ўзводзілася ў 122-128 гадах рымлянамі для абароны ад набегаў Брыгантыя і піктаў."
+        },
+      ],
+    },
+    {
+      sightId: 5,
+      imageId: "5-the-British-Museum.jpg",
+      smallImageId: "",
+      localizations: [
+        {
+          name: "British museum",
+          description:
+            "The British Museum, located in London, is the second most visited art museum, second only to the Louvre. You will see both mysterious exhibits and ancient artifacts, as well as the most famous objects of history."
+        },
+        {
+          name: "Британский музей",
+          description:
+            "Расположенный в Лондоне Британский музей является вторым по посещаемости художественным музеем, уступая только Лувру. Вы увидите как загадочные экспонаты и древнейшие артефакты, так и самые знаменитые объекты истории."
+        },
+        {
+          name: "Брытанскі музей",
+          description:
+            "Размешчаны ў Лондане Брытанскі музей з'яўляецца другім па наведвальнасці мастацкім музеем, саступаючы толькі Луўру. Вы ўбачыце як загадкавыя экспанаты і старажытныя артэфакты, так і самыя знакамітыя аб'екты гісторыі."
+        },
+      ],
+    },
+    {
+      sightId: 6,
+      imageId: "6-Tower-Bridge.jpg",
+      smallImageId: "",
+      localizations: [
+        {
+          name: "Tower Bridge",
+          description:
+            "A partly drawbridge, partly a neo-Gothic suspension bridge over the Thames is located near the Tower. Another symbol of Britain, which lights up in the dark with hundreds of lights. From the bridge, you can watch the slowly sailing ships and take memorable photos."
+        },
+        {
+          name: "Тауэрский мост",
+          description:
+            "Частично разводной, частично подвесной мост в неоготическом стиле, проведенный над Темзой, расположен поблизости от Тауэра. Еще один символ Британии, загорающийся в темное время суток сотнями огней. С моста можно наблюдать за неспешно идущими суднами и делать памятные фото."
+        },
+        {
+          name: "Таўэрскі мост",
+          description:
+            "Часткова развадны, часткова падвясны мост у неагатычным стылі, праведзены над Тэмзы, размешчаны паблізу ад Таўара. Яшчэ адзін сімвал Брытаніі, загараю ў цёмны час сутак сотнямі агнёў. З моста можна назіраць за павольна ідучымі суднамі і рабіць памятныя фота."
+        },
+      ],
+    },
+  ],
+  localizations: [
+    {
+      name: "England",
+      capital: "London",
+      description:
+        "England is an amazing country. The largest historical part of the United Kingdom, a state within a state. Homeland of the language, which has won international status, and the flag, which has become a cult art object. The cradle of the monarchy turned into a rating show. A land full of sights, with a long history living everywhere: on the streets of ancient cities, within the walls of ancient castles and fortresses. A fashionable Mecca with iconic boutiques and galleries. Concentration of megacities: London, Liverpool, Manchester, Birmingham - bright, boiling, modern, each with its own unique face."
+    },
+    {
+      name: "Англия",
+      capital: "Лондон",
+      description:
+        "Англия — страна удивительная. Крупнейшая историческая часть Соединенного Королевства, государство в государстве. Родина языка, завоевавшего статус международного, и флага, ставшего культовым арт-объектом. Колыбель монархии, превратившейся в рейтинговое шоу. Земля, переполненная достопримечательностями, с многовековой историей, живущей повсюду: на улицах старинных городов, в стенах древних замков и крепостей. Модная Мекка с культовыми бутиками и галереями. Сосредоточение мегаполисов: Лондон, Ливерпуль, Манчестер, Бирмингем — яркие, кипящие, современные, каждый — со своим уникальным лицом."
+    },
+    {
+      name: "Англія",
+      capital: "Лондон",
+      description:
+        "Англія - ​​краіна дзіўная. Найбуйнейшая гістарычная частка Злучанага Каралеўства, дзяржава ў дзяржаве. Радзіма мовы, які заваяваў статус міжнароднага, і сцяга, які стаў культавым арт-аб'ектам. Калыска манархіі, якая ператварылася ў рэйтынгавае шоў. Зямля, перапоўненая славутасцямі, з шматвяковай гісторыяй, якая жыве паўсюль: на вуліцах старадаўніх гарадоў, у сценах старажытных замкаў і крэпасцяў. Модная Мека з культавымі буцікамі і галерэямі. Засяроджванне мегаполісаў: Лондан, Ліверпуль, Манчэстар, Бірмінгем - яркія, кіпячыя, сучасныя, кожны - са сваім унікальным тварам."
+    },
+  ],
+};
