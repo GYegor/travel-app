276ae724abe7f9c8ea66cdd71694afcd3d82ed36
GYegor <yahor2011@gmail.com>
2021-03-17 00:27:35 +0300

    5

--- api/src/app.ts
+++ api/src/app.ts
@@ -19,7 +19,7 @@ app.use(express.json());
 
 app.use(requestLogMiddleware);
 
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, '../../frontend/build')));
 
 app.use('/favicon.ico', (req, res) => res.sendStatus(StatusCodes.NO_CONTENT));
 
@@ -31,7 +31,7 @@ app.use('/api/users', userRouter);
 app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../frontend/build', 'index.html'));
 });
 
 export = app;
