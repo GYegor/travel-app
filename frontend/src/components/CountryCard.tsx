699f9dde7a9c111947f75011bb99cd64a410eaa5
Shukevich Kseniya <Kseniya.Shukevich@gmail.com>
2021-03-16 22:45:24 +0300

    refactore: data

--- frontend/src/components/CountryCard.tsx
+++ frontend/src/components/CountryCard.tsx
@@ -21,7 +21,7 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
       transform: 'scale(1.1)',
       'background-color': 'rgb(250, 255, 240)',
     }
-    
+
   },
   action: {
     '&:hover': {
@@ -37,14 +37,14 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
 
 const CountryCard: React.FC<ICountryCard> = (props) => {
   const { name, capital, smallImageId, id } = props;
-  const classes = useStyles();  
+  const classes = useStyles();
 
   return (
     <Card className={classes.root}>
       <Link to={`/country/${id}`} className="card__link">
         <CardActionArea className={classes.action}>
           <Box
-            style={{ backgroundImage: `url('${cloudUrl}/${cloudName}/image/upload/h_150/${smallImageId}')` }}
+            style={{ backgroundImage: `url('${cloudUrl}/${cloudName}/image/upload/h_500/${smallImageId}')` }}
             className={classes.media}
           />
           <CardContent>
