b4d36686fa38eb1fc8e0bf6cd495710868743796
GYegor <yahor2011@gmail.com>
2021-03-17 18:41:35 +0300

    fix: remove build dirs

--- api/build/modules/users/user.model.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserModel = void 0;
-const mongoose = require("mongoose");
-const UserSchema = require("./user.schema");
-exports.UserModel = mongoose.model("User", UserSchema);
