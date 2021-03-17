436ba635b8fac87af2b963a3afd3dfe54d3fa156
Shukevich Kseniya <Kseniya.Shukevich@gmail.com>
2021-03-17 17:12:08 +0300

    Heroku

--- api/build/app.js
+++ api/build/app.js
@@ -14,13 +14,13 @@ app.use(cors());
 app.use(helmet({ contentSecurityPolicy: (process.env.NODE_ENV === 'production') ? undefined : false }));
 app.use(express.json());
 app.use(requestLogMiddleware);
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, '../../frontend/build')));
 app.use('/favicon.ico', (req, res) => res.sendStatus(http_status_codes_1.StatusCodes.NO_CONTENT));
 // Routers
 app.use('/api/countries', countryRouter);
 app.use('/api/users', userRouter);
 app.use(errorMiddleware);
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../frontend/build', 'index.html'));
 });
 module.exports = app;
